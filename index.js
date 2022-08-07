32ab892371a7184e07dbe0c4daa7e16ef038fa40
YOHAAN SINGLA <yohaan.singla@gmail.com>
2022-08-07 09:38:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Checking JS code
+var error = false
+var error_message = ""
+if (error==true) {
+ console.error(`Error found in ${error_message}!`)
+} else {
+ console.log("Good Job no Errors.")
+}
